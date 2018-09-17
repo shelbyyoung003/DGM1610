c57a7ab2135660ac8841564d54d52f50320986d4
Shelby <shelbyyoung003@gmail.com>
2018-09-17 15:47:42 -0600

    Variables w Button

---
+++ 1610/Assets/Scripts/AddMoney.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoney : MonoBehaviour
+{
+	public int Money = 10;
+
+	public void AddMoreMoney (int money)
+	{
+		Money += money;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
