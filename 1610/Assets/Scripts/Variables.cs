3c91e653d03b17b60d257890028a1eb3ee0d36e1
Shelby <shelbyyoung003@gmail.com>
2018-09-17 15:33:42 -0600

    Added Variables

---
+++ 1610/Assets/Scripts/Variables.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+
+	public bool IsAWizard;
+	
+	public int AmmoCount = 100;
+	public int InventoryCount;
+
+	public float Health = 1.0f;
+	public float Money;
+
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+	
+	// Use this for initialization
+	void Start () {
+		print(PlayerName);
+		print("has");
+		print("$" + Money);
+
+		if (IsAWizard)
+		{
+			print(PlayerName + "Is a Wizard.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Location;
+	}
+}
