6a66f35716b915da6585f6d0b2ffdae9e8be1d28
Shelby <shelbyyoung003@gmail.com>
2018-09-19 15:40:23 -0600

    FloatData

---
+++ 1610/Assets/Scripts/AddValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
+{
+    public FloatData ValueObj;
+    public FloatData MaxValue;
+    public FloatData MinValue;
+    
+
+    public UnityEvent EventMax;
+    public UnityEvent EventMin;
+
+    public void AddValueToObj(FloatData data)
+    {
+        ValueObj.Value += data.Value;
+        
+        if (ValueObj.Value >= MaxValue.Value)
+        {
+            EventMax.Invoke();
+        }
+
+        if (ValueObj.Value <= MinValue.Value)
+        {
+            EventMin.Invoke();
+        }
+        
+      
+    }  
+
+}
---
+++ 1610/Assets/Scripts/MouseDownBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseDownBehavior : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+}
