a859d97762b299d8c4ea49edf5f95d436b2b6c45
Shelby <shelbyyoung003@gmail.com>
2018-09-24 15:35:21 -0600

    MouseEvent

---
+++ 1610/Assets/Scripts/MouseEventBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEventBehaviour : MonoBehaviour
+{
+    public UnityEvent MouseDown;
+    public UnityEvent MouseUp;
+
+    private void OnMouseDown ()
+    {
+        MouseDown.Invoke();
+    }
+
+    private void OnMouseUp ()
+    {
+        MouseUp.Invoke();
+    }
+    
+}
