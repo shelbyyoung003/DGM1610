f6c4263584b3219ce78d2bbb341c2e23bfeaa35f
Shelby <shelbyyoung003@gmail.com>
2018-09-27 19:28:28 -0600

    Homework - Subtract Value

--- 1610/Assets/Scripts/SubtractValue.cs
+++ 1610/Assets/Scripts/SubtractValue.cs
@@ -7,9 +7,9 @@ public class SubtractValue : ScriptableObject
 
 	public FloatData Data;
     
-	public void OnSubtractValue (FloatData Data)
+	public void OnSubtractValue (FloatData outsideData)
 	{
-		Data.Value -= Data.Value;
+		outsideData.Value -= Data.Value;
 	}
 
 }
