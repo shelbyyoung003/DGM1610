45f73efbe1be6e069c56e55cb5939965e277e208
Shelby <shelbyyoung003@gmail.com>
2018-10-01 15:37:46 -0600

    If Statements and Else Clause

---
+++ 1610/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+public class IfStatements : MonoBehaviour
+{
+
+	public bool LightOn;
+	public string Password;
+	public int A, B, C;
+
+	public FloatData WizardPower, PawnPower;
+	public NameID Lock, Key;
+	
+	void Update () {
+
+		if (Lock == Key)
+		{
+			print("Open Door");
+		}
+
+		if (LightOn)
+		{
+			print("The light is on.");
+		}
+		else
+		{
+			print("The light is off.");
+		}
+
+		if (Password == "OU812")
+		{
+			print("You are correct.");
+		}
+
+		if (C == A + B)
+		{
+			print(C);
+		}
+		
+		if (WizardPower.Value > PawnPower.Value)
+		{
+			print("Wizard wins.");
+
+		}
+
+	}
+}
