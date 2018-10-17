a3e837f7807be0042838adb4bab44807cd48b658
Shelby <shelbyyoung003@gmail.com>
2018-10-17 15:33:35 -0600

    Added Health Bar

---
+++ 1610/Assets/Scripts/ValueChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+
+	public FloatData Data;
+
+	public void AddValue(FloatData obj)
+	{
+		Data.Value += obj.Value;
+	}
+	
+	public void SubtractValue(FloatData obj)
+	{
+		Data.Value -= obj.Value;
+	}
+
+}
