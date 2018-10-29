6445e4bcb08638577f80bd79523f63b5e9bf750c
Shelby <shelbyyoung003@gmail.com>
2018-10-29 15:06:45 -0600

    Enums and Switches

---
+++ 1610/Assets/Scripts/GameStates.cs
@@ -0,0 +1,14 @@
+﻿public class GameStates {
+
+	public enum States
+	{
+		Starting,
+		Loading,
+		Playing,
+		Dying,
+		Ending
+	}
+
+	public States State;
+	
+}
---
+++ 1610/Assets/Scripts/SwitchStatements.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchStatements : MonoBehaviour
+{
+	public GameStates.States GameState;
+	public UnityEvent Starting, Loading, Playing, Dying, Ending;
+	
+	// Update is called once per frame
+	void Update () {
+		switch (GameState)
+		{
+				case GameStates.States.Starting:
+					print("We are starting the game!");
+					break;
+				
+				case GameStates.States.Loading:
+					print("We are loading the game!");
+					break;
+				
+				case GameStates.States.Playing:
+					print("We are playing the game!");
+					break;
+				
+				case GameStates.States.Dying:
+					print("You have died.");
+					break;
+				
+				case GameStates.States.Ending:
+					print("We are ending the game!");
+					break;
+		}
+		
+	}
+}
