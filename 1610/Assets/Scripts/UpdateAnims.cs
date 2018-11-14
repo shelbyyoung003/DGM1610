3b00998fcb3ad16d86395983dd95c26b3cb855dd
Shelby <shelbyyoung003@gmail.com>
2018-11-14 15:16:30 -0700

    Scripting Animation Controllers

---
+++ 1610/Assets/Scripts/UpdateAnims.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UpdateAnims : MonoBehaviour
+{
+
+	private Animator anims;
+	public float Speed;
+
+	void Start ()
+	{
+		anims = GetComponent<Animator>();
+
+	}
+	
+	void Update () {
+		
+		anims.SetFloat("Speed", Speed);
+		
+	}
+}
